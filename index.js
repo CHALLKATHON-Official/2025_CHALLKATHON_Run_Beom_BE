5b8b7898cbde04488459e04772b47b2e36e72460
Beomjang <ibjang05@naver.com>
2025-06-24 03:44:38 +0900

    🐛 Fix route pattern to avoid path-to-regexp error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'Feindex')));
 
-app.get('/*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'Feindex', 'index.html'));
 });
 
