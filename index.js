2688c0adb5bd9e7484ec504015ad9fd55ebd943b
Beomjang <ibjang05@naver.com>
2025-06-24 03:41:27 +0900

    🐛 Fix path-to-regexp error with correct route pattern

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('Feindex'));
+app.use(express.static(path.join(__dirname, 'Feindex')));
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/Feindex/index.html');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Feindex', 'index.html'));
 });
 
 app.listen(PORT, () => {
